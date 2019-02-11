16e55ee4733730453b4160e31c09340f9aa07b29
anast29 <ivanovaanast29@gmail.com>
2019-02-11 16:55:49 +0200

    WS && Node.js Express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const WebSocketServer = require('ws').Server,
-    webSocket = new WebSocketServer({port: 8000});
-
+    webSocket = new WebSocketServer({port: 8000}, {perMessageDeflate: false});
+const JSON = require('circular-json');
 webSocket.on('connection', function (ws) {
     ws.onmessage = function(message) {
-        console.log(message);
+        const msg = JSON.stringify(message);
+        console.log(JSON.parse(msg));
     };
-
     ws.onclose = function() {
 
     };
